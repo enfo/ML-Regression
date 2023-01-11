3c9a730845409873acca24eba2912dc782d14dfb
Christian Borg <cborg@microsoft.com>
2023-01-11 19:54:07 +0100

    another test

--- tests/test_Regression.py
+++ tests/test_Regression.py
@@ -1,9 +1,10 @@
 import sys
 
-sys.path.append("../src")
+# sys.path.append("../src")
 
-
-from MLFunctions import MLRegressions as ml
+# from MLFunctions import MLRegressions as ml
+# from ..src.MLFunctions
+import src.MLFunctions.MLRegressions as ml
 
 
 def test_answer():
