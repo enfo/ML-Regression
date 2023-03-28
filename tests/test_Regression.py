51f2df87c05b03d9a8f1540a3f543d1f31933528
Christian Borg <cborg@microsoft.com>
2023-03-28 14:17:23 +0200

    compare accuracy to sklearn

--- tests/test_Regression.py
+++ tests/test_Regression.py
@@ -6,6 +6,9 @@ import numpy as np
 # from MLFunctions import MLRegressions as ml
 # from ..src.MLFunctions
 import src.MLFunctions.MLRegressions as ml
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from pytest import approx
 
 
 def test_MSError():
@@ -17,19 +20,14 @@ def test_MSError():
 
 
 def test_dataScaling():
-    assert (
-        ml.scaleData(np.array([[1.0], [2.0], [3.0], [4.0], [5.0]]))
-        == [[0], [0.25], [0.5], [0.75], [1]]
-    ).all() == True
 
-    assert (
-        ml.scaleData(np.array([[1.0, 10], [2.0, 20], [3.0, 30], [4.0, 40], [5.0, 50]]))
-        == [[0, 0], [0.25, 0.25], [0.5, 0.5], [0.75, 0.75], [1, 1]]
-    ).all() == True
+    assert ml.scaleData(
+        np.array([[1.0, 10], [2.0, 20], [3.0, 30], [4.0, 40], [5.0, 50]])
+    ) == approx(np.array([[0, 0], [0.25, 0.25], [0.5, 0.5], [0.75, 0.75], [1, 1]]))
 
 
 def test_sigmoid():
-    assert ml.sigmoid(0) == 0.5
+    assert ml.sigmoid(0) == approx(0.5)
     assert ml.sigmoid(1) == 0.7310585786300049
     assert ml.sigmoid(-1) == 0.2689414213699951
 
@@ -43,3 +41,59 @@ def test_gradientDescentIdentical():
     )
     assert updateWeights == 0
     assert updateC == 0
+
+
+def test_gradientDescentUpdate1():
+    updateWeights, updateC = ml.gradientDesecent(
+        np.array([[1.0], [2.0], [3.0], [4.0], [5.0]]),
+        [1.0, 2.0, 3.0, 4.0, 5.0],
+        [0.0, 1.0, 2.0, 3.0, 4.0],
+        0.1,
+    )
+    assert updateWeights == approx(np.array([-0.3]))
+    assert updateC == approx(-0.1)
+
+
+def predictData(X, weigths, c):
+    n = len(X)
+    dimensions = X.shape[1]
+
+    YPredClass = []
+
+    for i in range(0, n):
+        currentValue = 0
+        for dim in range(0, dimensions):
+            currentValue = currentValue + X[i][dim] * weigths[dim]
+        currentValue += c
+
+        if currentValue > 0.5:
+            YPredClass.append(1)
+        else:
+            YPredClass.append(0)
+
+    return YPredClass
+
+
+def test_CompareToSklear():
+    dimensions = 2
+    numberOfSamples = 100
+    learningRate = 0.1
+    iterations = 10000
+
+    X, Y = ml.getLogisticRegressionDemoData(numberOfSamples, dimensions)
+    Xscaled = ml.scaleData(X)
+    Yscaled = ml.scaleData(Y)
+
+    weigths, c = ml.runLogisticRegression(Xscaled, Yscaled, learningRate, iterations)
+
+    yPredicted = predictData(Xscaled, weigths, c)
+    acc_score = accuracy_score(Y, yPredicted, normalize=True)
+
+    reg = LogisticRegression().fit(Xscaled, Yscaled)
+    scoreSKLearn = reg.score(Xscaled, Yscaled)
+
+    print(reg.coef_)
+    print(reg.intercept_)
+    print(weigths)
+
+    assert acc_score == approx(scoreSKLearn, 2.0)
